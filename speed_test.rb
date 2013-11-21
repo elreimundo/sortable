fa4ecf2b9968c8a048285f90d8fc92a069ddb797
David Reiman <david.reiman@gmail.com>
2013-11-21 00:28:30 -0800

    adds speed test to demonstrate relative efficiency

---
+++ speed_test.rb
@@ -0,0 +1,17 @@
+require 'benchmark'
+require_relative 'sortable.rb'
+
+class Array
+	include Sortable
+end
+
+SORTABLE_METHODS = [:insertion_sort, :selection_sort, :bubble_sort, :shell_sort, :merge_sort]
+
+n = ARGV[0] ? ARGV[0].to_i : 10000
+test_array = Array.new(n){rand(100)}
+
+Benchmark.bm(10) do |x|
+	SORTABLE_METHODS.each do |method|
+		x.report(method.to_s.split('_')[0]) {test_array.send(method)}
+	end
+end
